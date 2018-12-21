2b8bf0e1d4feb79df54606f0cfbd620dd40c334a
Beau Hansen <beau.hansen45@gmail.com>
2018-12-21 15:37:43 -0600

    added gametag class

---
+++ VaporAPI/VaporAPI.Library/GameTag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VaporAPI.Library
+{
+    public class GameTag
+    {
+        public int GameId { get; set; }
+
+        public int TagId { get; set; }
+
+
+    }
+}
