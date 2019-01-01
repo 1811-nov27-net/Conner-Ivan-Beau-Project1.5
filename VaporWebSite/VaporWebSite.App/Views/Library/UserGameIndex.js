9f0ceb525315059a7189b8914fd1429e08930ce2
Ivan <alvarezivan001@yahoo.com>
2019-01-01 17:50:53 -0600

    some view chagnes and attempted java

---
+++ VaporWebSite/VaporWebSite.App/Views/Library/UserGameIndex.js
@@ -0,0 +1,21 @@
+﻿'use strict';
+
+
+//window.onload = function () {
+
+//    document.onclick = function () {
+
+//        let item = document.getElementById("modelitem")
+
+//        item.onclick = function () {
+//            let name = document.getElementById("modelitemname")
+//            let gameid = document.getElementById("gameid")
+//            let price = document.getElementById("price")
+//            let desc = document.getElementById("description")
+//            gameid.style.display = "block";
+//        }
+//        window.onclick = function () {
+//            gameid.style.display = "none";
+//        }
+//    }
+//}
