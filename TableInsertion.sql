9eca4ba3ceda99d83f1e5aa776da5a6ed5360241
Conner Knight <conneraknight@gmail.com>
2019-01-02 19:04:42 -0600

    fixed purchasing game

--- TableInsertion.sql
+++ TableInsertion.sql
@@ -2,3 +2,5 @@ INSERT INTO Developer(Name,FoundingDate,Website)
 VALUES ('Bethesda','1986','https://bethesda.net');
 
 SELECT * FROM Game
+
+select * from UserDlc
--- VaporWebSite/VaporWebSite.App/wwwroot/Scripts/Purchase.js
+++ VaporWebSite/VaporWebSite.App/wwwroot/Scripts/Purchase.js
@@ -4,19 +4,32 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let dropdown = document.getElementById("tax");
     let gameprice = document.getElementById("game-price");
-    let wallet = document.getElementById("wallet");
+    let wallet = document.getElementById("initial-wallet");
     let totalprice = document.getElementById("total-price");
     let remaining = document.getElementById("remaining");
+    let output = document.getElementById("User_Wallet");
 
+    calculate();
 
-    dropdown.addEventListener("change", () => {
+    function calculate() {
         let tax = dropdown.options[dropdown.selectedIndex].value;
         let total = (1 + Number(tax)) * Number(gameprice.value);
         totalprice.innerHTML = total;
-        let rem = Number(wallet.value) - total;
+        let rem = Number(wallet.innerHTML) - total;
         remaining.innerHTML = rem;
+        output.value = rem;
+    }
 
 
+    dropdown.addEventListener("change", () => {
+        calculate();
+
+
+
+    });
+
+    dropdown.addEventListener("load", () => {
+        calculate();
 
 
     });
